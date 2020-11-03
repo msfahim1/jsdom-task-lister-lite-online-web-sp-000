2b23d0537513647811bcdb013bb668cf7f1da14d
Michael Fahim <michael_fahim@yahoo.com>
2020-11-03 01:09:29 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const createTaskForm = document.getElementById("create-task-form");
+  createTaskForm.addEventListener("submit", createNewTask);
+  console.log("hello")
 });
+
+
+const createNewTask = event => {
+  event.preventDefault();
+
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
